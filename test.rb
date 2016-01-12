ce83fd4b3aa8aa7362a297ded457ff50026272a7
Karb0 <carbonel.mathieu@gmail.com>
2016-01-12 16:59:06 +0100

    reverse test

--- test.rb
+++ test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require "./carre.rb"
 
 class TestCarre < Minitest::Test
 
@@ -7,32 +8,26 @@ class TestCarre < Minitest::Test
   end
 
   def test_2
-    skip
     assert_equal square_sum([4]), 16 
   end
 
   def test_3
-    skip
     assert_equal square_sum([2, 2]), 8
   end
 
   def test_4
-    skip
     assert_equal square_sum([4, 4]), 32
   end
 
   def test_5
-    skip
     assert_equal square_sum([2, 3, 4]), 29
   end
 
   def test_6
-    skip
     assert_equal square_sum([4, 9, 3, 7]), 155
   end
 
   def test_7
-    skip
     assert_equal square_sum([12, 3, 7, 9, 22, 100, 476]), 237343
   end
 
